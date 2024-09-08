75a42824b440ddb424c75f7ed50f2bb07fab2397
Marouane <57333995+riimuru@users.noreply.github.com>
2024-09-07 20:39:57 -0400

    refactor(storage-handler): store all user settings in local storage

--- src/content-scripts/storage-handler.ts
+++ src/content-scripts/storage-handler.ts
@@ -2,8 +2,6 @@ import { UserSettings } from '@/types';
 import { getFromStorage } from '@/utils';
 
 getFromStorage<UserSettings>('algoArchive', (result) => {
-  if (result && result.githubAccessToken) {
-    console.log('GitHub Access Token:', result.githubAccessToken);
-    localStorage.setItem('githubAccessToken', result.githubAccessToken);
-  }
+  if (result && result.githubAccessToken)
+    localStorage.setItem('algoArchive', JSON.stringify(result));
 });
